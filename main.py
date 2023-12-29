9f082ad5fa66b350176fcc9e51fa1fb9cb815df8
Patrick D Faerber <patrickdfaerber@gmail.com>
2023-12-29 15:23:25 -0500

    Add README from instructions and key/value inputs

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # from replit import clear
-from art import logo
 #HINT: You can call clear() to clear the output in the console.
+from art import logo
+
 
 print(logo)
+name = input("What is your name? ")
+bid = input("What is your Bid? ")
+
+def get_bidder(name, bid):
+    bidder_dictionary = {}
+    bidder_dictionary["name"] = name
+    bidder_dictionary["bid"] = bid
+
+get_bidder(name, bid)
+
