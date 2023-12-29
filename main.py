31a189ff8f359559d7cbd961e6e7110efb022792
Patrick D Faerber <patrickdfaerber@gmail.com>
2023-12-29 16:07:26 -0500

    Add some print formatting

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ while auction:
     bidder_dictionary = {"name": name, "bid": bid}
     additional_bidders = input("Are there any other bidders? Type 'yes' or 'no' ")
     if additional_bidders == 'no':
-        print(f"The winner is {bidder_dictionary['name']} with a bid of {bidder_dictionary['bid']} \n \n")
+        print(f"The winner is {bidder_dictionary['name']} with a bid of ${bidder_dictionary['bid']}")
     auction = False
 
-print(bidder_dictionary)
+print(f"\n\n{bidder_dictionary}")
 
 
 # add_bidder(name, bid)
