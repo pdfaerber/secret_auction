b910f2b0a66cc5849004083e7df0950eb833511a
Patrick D Faerber <patrickdfaerber@gmail.com>
2024-01-01 12:03:50 -0500

    Add condition to find max bid

--- main.py
+++ main.py
@@ -13,15 +13,19 @@ while auction:
     bid = int(input("What is your bid? "))
     new_bidder = {"name": name, "bid": bid}
     bidder_list.append(new_bidder)
-    # see dictionary items added to list
+    # see dictionary items added to list (supposed to be hidden, but I want to see)
     print(bidder_list)
     additional_bidders = input("Are there any other bidders? Type 'yes' or 'no' ")
     if additional_bidders == 'yes':
         auction
     else:
+        max_bid = 0
         for item in bidder_list:
-            print(item['bid'])
-        print(f"The winner is {bidder_list[0]['name']} with a bid of ${bidder_list[0]['bid']}")
+            if item['bid'] > max_bid:
+                max_bid = (item['bid'])
+            # print(max_bid)
+
+        print(f"The winner is {bidder_list[0]['name']} with a bid of ${max_bid}")
         auction = False
 
 
